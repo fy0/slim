737b448b21bd550b8e099b6d61c0a33f57c4c1e4
fy <fy0@qq.com>
2018-04-03 23:20:44 +0800

    update

--- slim/base/ws.py
+++ slim/base/ws.py
@@ -4,7 +4,7 @@ import aiohttp
 import asyncio
 from aiohttp import web
 from aiohttp.web_request import BaseRequest
-from slim.base.user import BaseUserMixin
+from slim.base.user import BaseUserMixin, BaseUser
 from slim.retcode import RETCODE
 from ..utils import MetaClassForInit, async_call
 
@@ -15,23 +15,48 @@ class WSRouter(BaseUserMixin, metaclass=MetaClassForInit):
     heartbeat_timeout = 30
     connections = set()
     _on_message = {}
+    users = {}
 
     def __init__(self):
         self.access_token = None
+        self._current_user = None
 
     def setup_user_key(self, key, expires=30):
         self.access_token = key
+        user = self.current_user
+        if user:
+            self.users.setdefault(user.id, [])
+            self.users[user.id].append(self)
 
     def teardown_user_key(self):
+        user = self.current_user
+        if user: self.users[user.id].remove(self)
         self.access_token = None
+        self._current_user = None
 
     def get_current_user(self):
         if self.access_token is not None:
             return self.get_user_by_key(self.access_token)
 
+    async def send_json(self, data, compress=None, *, dumps=json.dumps):
+        self.ws.send_json(data, compress=compress, dumps=dumps)
+
+    async def send_str(self, data, compress=None):
+        self.ws.send_str(data, compress=compress)
+
+    async def send_bytes(self, data, compress=None):
+        self.ws.send_bytes(data, compress=compress)
+
+    @property
+    def current_user(self) -> BaseUser:
+        if not self._current_user:
+            self._current_user = self.get_current_user()
+        return self._current_user
+
     @classmethod
     def cls_init(cls):
         cls.connections = set()
+        cls.users = {}
         if len(cls._on_message) > 0:
             cls._on_message = cls._on_message.copy()
         else:
@@ -71,28 +96,25 @@ class WSRouter(BaseUserMixin, metaclass=MetaClassForInit):
                             logger.error('websocket command parse failed %s: %r' % (msg.data, wsid))
                             continue
 
-                        def send_json_wrap(rid):
-                            async def send_json(code, data=NotImplemented):
-                                if data is NotImplemented:
-                                    data = RETCODE.txt_cn.get(code)
-                                val = {'code': code, 'data': data}
-                                logger.info('websocket reply %r - %s: %r' % (command, val, wsid))
-                                await ws.send_json([rid, val])
-
-                            return send_json
-
-                        send_json = send_json_wrap(rid)
+                        def make_send_json(rid):
+                            async def _send_json(data):
+                                logger.info('websocket reply %r - %s: %r' % (command, data, wsid))
+                                await ws.send_json([rid, data])
+                            return _send_json
+                        send = make_send_json(rid)
 
                         if command in self._on_message:
                             logger.info('websocket command %r - %s: %r' % (command, data, wsid))
                             for i in self._on_message[command]:
-                                ret = await async_call(i, self, send_json, data)
+                                ret = await async_call(i, self, send, data)
                                 '''
-                             def ws_command_test(wsr: WSRouter, send_json, data):
+                             def ws_command_test(wsr: WSRouter, send, data):
                                 pass
                              '''
-                                if ret is not None:
-                                    await send_json(*ret)
+                                await send({
+                                    'code': RETCODE.WS_DONE,
+                                    'data': ret
+                                })
                         else:
                             logger.info('websocket command not found %s: %r' % (command, wsid))
 
@@ -113,18 +135,22 @@ class WSRouter(BaseUserMixin, metaclass=MetaClassForInit):
 
 
 @WSRouter.route('hello')
-def ws_command_signin(wsr: WSRouter, send_json, data):
-    send_json(RETCODE.SUCCESS, 'Hello Websocket!')
-    return RETCODE.SUCCESS, 'Hello Again!'
+async def ws_command_signin(wsr: WSRouter, send, data):
+    await send('Hello Websocket!')
+    return 'Hello Again!'
 
 
 @WSRouter.route('signin')
-def ws_command_signin(wsr: WSRouter, send_json, data):
-    wsr.setup_user_key(data['access_token'])
+async def ws_command_signin(wsr: WSRouter, send, data):
+    if 'access_token' in data:
+        wsr.setup_user_key(data['access_token'])
+        user = wsr.current_user
+        if user: logger.debug('websocket user signin: %s' % user)
     return RETCODE.SUCCESS
 
 
 @WSRouter.route('signout')
-def ws_command_signin(wsr: WSRouter, send_json, data):
+async def ws_command_signout(wsr: WSRouter, send, data):
+    user = wsr.current_user
+    if user: logger.debug('websocket user signout: %s' % user)
     wsr.teardown_user_key()
-    return RETCODE.SUCCESS
