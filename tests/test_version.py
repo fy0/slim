643cbf7e89d886fb269453f80cf52c9c33603fef
fy <fy0748@gmail.com>
2020-11-06 01:25:00 +0800

    version update

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,7 +5,7 @@ import slim
 
 
 def test_version():
-    setup_file = open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'setup.py')), 'r', encoding='utf-8').read()
+    setup_file = open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'pyproject.toml')), 'r', encoding='utf-8').read()
 
-    m = re.search(r"version='(.+?)',", setup_file)
+    m = re.search(r"version = \"(.+?)\"", setup_file)
     assert m.group(1) == slim.__version__
